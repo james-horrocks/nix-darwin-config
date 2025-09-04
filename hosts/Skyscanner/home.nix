610e686ffc6d68f21cc93292f031e5831040eddc
James Horrocks <jameshorrocks@skyscanner.net>
2025-09-04 12:09:43 +0100

    Fix identityAgent path syntax in SSH configuration

--- hosts/Skyscanner/home.nix
+++ hosts/Skyscanner/home.nix
@@ -42,7 +42,7 @@
     enableDefaultConfig = false;
     matchBlocks = {
       "*" = {
-        identityAgent = "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+        identityAgent = "'~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock'";
       };
     };
   };
